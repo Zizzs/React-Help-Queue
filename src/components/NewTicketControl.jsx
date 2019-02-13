b86ea59d2059f356c09bb34a3e0d15c98d733026
Alex Williams <Zizzs17@gmail.com>
2019-02-13 11:46:45 -0800

    Fully working Ticket Queue, allows the user to create a ticket and view it on the home page.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from "prop-types";
 
 class NewTicketControl extends React.Component {
 
@@ -14,13 +15,12 @@ class NewTicketControl extends React.Component {
     
     handleTroubleshootingConfirmation(){
         this.setState({formVisibleOnPage: true});
-        console.log("clicked");
     }
 
     render(){
         let currentlyVisibleContent = null;
         if (this.state.formVisibleOnPage) {
-            currentlyVisibleContent = <NewTicketForm />;
+            currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
         } else {
             currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
         }
@@ -30,6 +30,10 @@ class NewTicketControl extends React.Component {
             </div>
         );
     }
+}
+
+NewTicketControl.propTypes = {
+    onNewTicketCreation: PropTypes.func
 }
 
 export default NewTicketControl;
